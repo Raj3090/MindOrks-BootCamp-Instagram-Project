118c4db47160ebc6a6dcd68ea9131a0453a51dbe
raj <rajkumaryadav3009@gmail.com>
2019-03-09 13:11:45 +0530

    mail and password validation in login and sign up

--- presentation/src/main/java/com/raj/sharephoto/login/LoginActivity.kt
+++ presentation/src/main/java/com/raj/sharephoto/login/LoginActivity.kt
@@ -1,10 +1,8 @@
 package com.raj.sharephoto.login
 
-import android.app.Activity
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.text.TextUtils
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
@@ -34,12 +32,38 @@ class LoginActivity : AppCompatActivity() {
             false
         })
 
+        email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if(s.isNotEmpty()){
+                    cancel_mail.visibility= View.VISIBLE
+                }else{
+                    cancel_mail.visibility= View.GONE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
+        mail_container.setBackgroundResource(R.drawable.login_edittext_rounded_bcg)
+
+        password.setBackgroundResource(R.drawable.login_edittext_rounded_bcg)
+
+
         email_sign_in_button.setOnClickListener {
-                attemptLogin()
+            attemptLogin()
+        }
+
+        cancel_mail.setOnClickListener {
+            email.setText("")
         }
 
         sign_up_button.setOnClickListener {
             startActivity(getSignUpActivityIntent())
+            finish()
         }
 
     }
@@ -57,11 +81,11 @@ class LoginActivity : AppCompatActivity() {
         val passwordValid = loginVM.isPasswordValid(password = passwordStr)
 
         if (!emailValid){
-
+            mail_container.setBackgroundResource(R.drawable.login_edittext_error_bcg)
         }
 
         if (!passwordValid){
-
+            password.setBackgroundResource(R.drawable.login_edittext_error_bcg)
         }
     }
 
--- presentation/src/main/java/com/raj/sharephoto/signup/SignUpActivity.kt
+++ presentation/src/main/java/com/raj/sharephoto/signup/SignUpActivity.kt
@@ -2,12 +2,14 @@ package com.raj.sharephoto.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProviders
 import com.raj.sharephoto.R
 import com.raj.sharephoto.common.getLoginActivityIntent
-import com.raj.sharephoto.login.LoginViewModel
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -32,30 +34,54 @@ class SignUpActivity : AppCompatActivity() {
 
         login_button.setOnClickListener {
             startActivity(getLoginActivityIntent())
+            finish()
         }
+
+        cancel_mail.setOnClickListener {
+            email.setText("")
+        }
+
+        email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if(s.isNotEmpty()){
+                    cancel_mail.visibility= View.VISIBLE
+                }else{
+                    cancel_mail.visibility= View.GONE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
     }
 
     private fun attemptSignUp() {
         // Store values at the time of the login attempt.
         val emailStr = email.text.toString()
         val passwordStr = password.text.toString()
-        val name = name.text.toString()
+        val nameStr = name.text.toString()
 
         val emailValid = signUpVM.isEmailValid(emailStr)
 
         val passwordValid = signUpVM.isPasswordValid(password = passwordStr)
 
-        val nameValid = signUpVM.isNameValid(name = name)
+        val nameValid = signUpVM.isNameValid(name = nameStr)
 
         if (!emailValid){
+            mail_container.setBackgroundResource(R.drawable.login_edittext_error_bcg)
 
         }
 
         if (!passwordValid){
+            password.setBackgroundResource(R.drawable.login_edittext_error_bcg)
 
         }
 
         if (!nameValid){
+            name.setBackgroundResource(R.drawable.login_edittext_error_bcg)
 
         }
 
