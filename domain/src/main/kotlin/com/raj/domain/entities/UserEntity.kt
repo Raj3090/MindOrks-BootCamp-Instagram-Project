c0f1bb2911bf3a1b1285d0449fd4d2278c9f2629
raj <rajkumaryadav3009@gmail.com>
2019-04-13 17:28:42 +0530

    remove directory

--- domain/src/main/kotlin/com/raj/domain/entities/UserEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.raj.domain.entities
-
-data class UserEntity(
-
-    var name: String,
-    var age: String?
-
-
-)
--- domain/src/main/kotlin/com/raj/domain/repositories/user/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.raj.domain.repositories.user
-
-import com.raj.domain.entities.UserEntity
-import io.reactivex.Observable
-
-interface UserRepository{
-    fun getUser(): Observable<UserEntity>
-}
